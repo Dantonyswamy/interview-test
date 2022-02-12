1078bcbe20b1db4adba64e317598d622e30ef62c
Dantony <dantonyswamy@realtermenergy.com>
2022-02-12 17:31:29 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const API_URL = 'http://localhost:8099'
+// const API_URL = 'http://localhost:8099'
 
 function App() {
   return (
