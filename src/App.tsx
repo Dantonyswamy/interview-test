2c24ace40d301a40ebd339eeb962f336fc5aec51
Dantony <dantonyswamy@realtermenergy.com>
2022-02-17 22:14:28 -0500

    Combined arrays and added list headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,70 @@
-import React, {useState, useEffect} from 'react';
-
-
+import React, {useState, useEffect, useMemo} from 'react';
 import { useAppDispatch, useAppSelector } from './hooks/hooks';
 import { requestKidsData } from './slice/kids';
 import { requestAdultsData } from './slice/adults';
 import { RootState } from './state';
 import { User } from './types/users';
-import { Button, Input, Card, Heading, ListItem } from './components';
+import { Button, Input, Card, Heading, ListItem, ListHeader  } from './components';
 import { BaseLayout, Container } from './layouts';
 
 
-export const App = () => {
-  
-  const [kids, setKids] = useState<User[]>([]);
-  const [adults, setAdults] = useState<User[]>([]);
-
+export const App = () => {  
+  const [minAge, setMinAge] = useState(0);
+  const [maxAge, setMaxAge] = useState(80);
+  const [combinedUsersList, setCombinedUsersList] = useState<User[]>([]);
   const dispatch = useAppDispatch();
   const kidsDataFromApi = useAppSelector((state: RootState) => state.kidsData);
   const adultsDataFromApi = useAppSelector((state: RootState) => state.adultsData);
-  useEffect(() => { 
-    setKids(kidsDataFromApi.kids);
-    setAdults(adultsDataFromApi.adults);
+
+  useEffect(() => {
+     setCombinedUsersList(kidsDataFromApi.kids.concat(adultsDataFromApi.adults))
   }, [kidsDataFromApi, adultsDataFromApi]);
   
+  const filteredUsersList = useMemo(() => {
+    return combinedUsersList.filter(user => user.age >= minAge && user.age <= maxAge);
+  }, [combinedUsersList, minAge, maxAge]);
 
+  const retriveUsers =  async() => { 
+    dispatch(requestKidsData());
+    dispatch(requestAdultsData());
+   ;
+  }
 
-  const kidsList = kids.map((kid,i) => {
-    return (    
-        <ListItem key={i} name={kid.name} age={kid.age} />       
-      
-    );
-  });
+  const changeMinAge = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMinAge(Number(event.target.value));
+  }
+    const changeMaxAge = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMaxAge(Number(event.target.value));
+  }
   
-  const adultsList = adults.map((adult,i) => {
+  const usersList = filteredUsersList.map((user,i) => {
     return (    
-        <ListItem key={i} name={adult.name} age={adult.age} />       
+        <ListItem key={i} name={user.name} age={user.age} />      
       
     );
   });
 
 
-  return (  
-
-      <BaseLayout> 
+  return (
+    <BaseLayout> 
       <Container>
          <Heading>Users</Heading> 
-      <Card>          
-         
-          <Input required type="text" label="Min Age:" id="min-age" defaultValue="20" />
-          <Input required type="text" label="Max Age:" id="min-age" defaultValue="40" />
-           <Button onClick={()=>dispatch(requestAdultsData())}>Retrive Users</Button> 
+      <Card>        
+          <Input required type="text" label="Min Age:" id="min-age" value={minAge} onChange={changeMinAge}/>
+          <Input required type="text" label="Max Age:" id="min-age" value={maxAge} onChange={changeMaxAge} />
+           <Button onClick={()=>retriveUsers()}>Retrive Users</Button> 
           
         </Card>
         </Container>  
         <Container>
          
       <Card>         
-          <Input required type="text" id="max-age" placeholder='Search...' />          
-          {adultsList.sort((a, b) => {
+          <Input required type="text" id="max-age" placeholder='Search...' /> 
+          <ListHeader />
+          {usersList.sort((a, b) => {
             if(a.props.name === b.props.name) return b.props.age - a.props.age;
             return a.props.name.localeCompare(b.props.name);
-          })}
-                
+          })}                
         
         </Card>
        </Container>  
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -8,7 +8,7 @@ interface ListItemProps extends React.InputHTMLAttributes<HTMLInputElement>{
 
 const CheckBox = styled.input.attrs({ type: "checkbox" })``;
 
-const Row = styled.div`
+export const Row = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -16,8 +16,22 @@ const Row = styled.div`
     padding: 10px;
     border-bottom: 1px solid #dadce0;
 `;
-
-const Content = styled.div`
+export const HeaderRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;  
+    border-bottom: 1px solid #dadce0;
+`;
+export const ListTitle = styled.div`
+    font-size: 20px;
+  font-weight: bold;
+  text-align: start;
+  font-family: inherit;
+ 
+`;
+export const Content = styled.div`
 font-size: 20px;
 font-family: inherit;
 margin-left: 15px;
@@ -31,6 +45,15 @@ export const ListItem: React.FC<ListItemProps> = ({ name, age }) => {
           <Content>{ name }</Content>
        <Content>{ age }</Content>
     </Row>
+  );
+}
+
+export const ListHeader: React.FC = () => {
+  return (
+      <HeaderRow>        
+          <ListTitle style={{marginLeft:'100px'}}>Name</ListTitle>
+       <ListTitle>Age</ListTitle>
+    </HeaderRow>
   );
 }
 
